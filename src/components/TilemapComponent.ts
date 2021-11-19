2469697be701f41862cca9989eb3042eea8b7f98
Gerolmed <werwolf420@yahoo.de>
2021-11-19 16:05:52 +0100

    Fixed generate method

--- src/components/TilemapComponent.ts
+++ src/components/TilemapComponent.ts
@@ -2,17 +2,15 @@ import {Component} from "../Component";
 import {MeshComponent} from "./MeshComponent";
 import {BufferGeometry, Material} from "three";
 
+function generateGeometry(): BufferGeometry {
+    return new BufferGeometry()
+}
+
 export class TilemapComponent extends MeshComponent {
 
 
     constructor(materials: Material[]) {
         super(generateGeometry(), materials);
     }
-
-
-    private generateGeometry(): BufferGeometry {
-        return new BufferGeometry()
-    }
-
 
 }
