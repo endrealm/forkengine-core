567f5eaa745b71bc4d93e7c6f2853f8cba13df34
Gerolmed <werwolf420@yahoo.de>
2021-11-19 21:21:31 +0100

    Added tilemap

---
+++ src/components/tilemap/simple/TileComponent.ts
@@ -0,0 +1,17 @@
+import {Component} from "../../../Component";
+import {PlaneBufferGeometry} from "three";
+
+const TILE_GEOMETRY = new PlaneBufferGeometry();
+
+export class TileComponent extends Component {
+
+    constructor (
+    ) {
+        super("TileComponent");
+    }
+
+    public setTile() {
+
+    }
+
+}
---
+++ src/components/tilemap/simple/TilemapComponent.ts
@@ -0,0 +1,35 @@
+import {Component} from "../../../Component";
+import {TileComponent} from "./TileComponent";
+import {GameObject} from "../../../GameObject";
+
+export class TilemapComponent extends Component {
+
+    public readonly tileMatrix: TileComponent[][] = [];
+
+    constructor (
+        private readonly width: number,
+        private readonly height: number,
+    ) {
+        super("TilemapComponent");
+    }
+
+    start() {
+        this.generateTiles();
+    }
+
+    private generateTiles() {
+
+        for (let x = 0; x < this.width; x++) {
+            const row: TileComponent[] = [];
+            this.tileMatrix.push(row)
+
+            for (let y = 0; y < this.height; y++) {
+                const tile = new TileComponent();
+                this.getGameObject().getScene().addGameObject(new GameObject())
+                    .addComponent(tile)
+                row.push(tile)
+            }
+        }
+    }
+
+}
